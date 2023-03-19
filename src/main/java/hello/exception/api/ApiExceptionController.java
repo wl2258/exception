2696e73c5b434e758264eae011439624ef337327
wl2258 <77067383+wl2258@users.noreply.github.com>
2023-03-19 21:05:35 +0900

    feat: 스프링 제공 ExceptionResolver

--- src/main/java/hello/exception/api/ApiExceptionController.java
+++ src/main/java/hello/exception/api/ApiExceptionController.java
@@ -1,11 +1,15 @@
 package hello.exception.api;
 
+import hello.exception.exception.BadRequestException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @RestController
@@ -17,6 +21,21 @@ public class ApiExceptionController {
             throw new RuntimeException("잘못된 사용자");
         }
         return new MemberDto(id, "hello " + id);
+    }
+
+    @GetMapping("/api/response-status-ex1")
+    public String responseStatuseEx1() {
+        throw new BadRequestException();
+    }
+
+    @GetMapping("/api/response-status-ex2")
+    public String responseStatusEx2() {
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad", new IllegalArgumentException());
+    }
+
+    @GetMapping("/api/default-handler-ex")
+    public String defaultException(@RequestParam Integer data) {
+        return "ok";
     }
 
     @Data
